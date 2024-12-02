da38dcc33c687e90b57b0e3f36f0b7408f60402f
Rafael Ignacio Pinto <rafael-ignacio.pinto@knab.nl>
2024-12-02 14:49:09 +0100

    wip

--- tests/axpo.reports.application.tests/ReportGeneratorShould.cs
+++ tests/axpo.reports.application.tests/ReportGeneratorShould.cs
@@ -2,13 +2,6 @@ namespace axpo.reports.application.tests;
 
 public class ReportGeneratorShould
 {
-    private ReportGenerator? _sut;
-
-    [SetUp]
-    public void Setup() {
-        _sut = new(null, new FakeTimeProvider())
-    }
-
     [Test]
     public void Test1()
     {
