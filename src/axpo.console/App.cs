0b40b596e45e807ad752dddd4a038153805989f6
Rafael Ignacio Pinto <rafael-ignacio.pinto@knab.nl>
2024-12-03 02:51:48 +0100

    refactoring

---
+++ src/axpo.console/App.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using axpo.reports.application;
+using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Configuration;
+using Polly;
+using Serilog;
+
+namespace axpo.console;
+
+public class App(ReportGenerator reportGenerator, ILogger logger, IConfiguration configuration)
+{
+    public async ValueTask RunAsync(CancellationToken cancellationToken)
+    {
+        var intervalInMinutes = int.Parse(configuration.GetValue<string>("intervalInMinutes")!);
+        logger.Information(
+            "Initiating application with a time interval of {Minutes} minutes",
+            intervalInMinutes
+        );
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var reportData = await Policy<Result<IList<ReportData>>>
+                    .HandleResult(result => result.IsFailure)
+                    .WaitAndRetryAsync(12, sleepDurationProvider: _ => TimeSpan.FromSeconds(5), onRetry: (c, t) =>
+                    {
+                        var msg = c.Result.IsFailure ? c.Result.Error : c.Exception.Message;
+                        logger.Warning(new ApplicationException(msg), $"Failed to generate report. Retrying in {t:g}.");
+                    })
+                    .ExecuteAsync(reportGenerator.Generate);
+
+                if (reportData.IsFailure)
+                    throw new ApplicationException(reportData.Error);
+
+                CreateReportFile(reportData.Value);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Couldn't generate report data." );
+            }
+
+            logger.Information("Next execution at {timeStamp}.", TimeProvider.System.GetLocalNow().AddMinutes(intervalInMinutes));
+            await Task.Delay(TimeSpan.FromMinutes(intervalInMinutes), cancellationToken);
+        }
+
+        logger.Information("Application was cancelled.");
+    }
+
+    private void CreateReportFile(IList<ReportData> data)
+    {
+        var date = TimeProvider.System.GetLocalNow();
+        var filePath = configuration.GetValue<string>("filePath");
+        Directory.CreateDirectory(filePath!);
+        var fileName = $"{filePath}PowerPosition_{date.AddDays(1):yyyyMMdd}_{date.DateTime:yyyyMMddHHmm}.csv";
+        var content = new StringBuilder();
+        content.AppendLine("Datetime;Volume");
+
+        foreach (var reportData in data)
+        {
+            content.AppendLine($"{reportData.Datetime:O};{reportData.Volume:F}");
+        }
+
+        File.WriteAllText(fileName, content.ToString());
+        logger.Information("Report {@fileName} created.", fileName);
+    }
+}
--- src/axpo.console/Program.cs
+++ src/axpo.console/Program.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
-using axpo.reports.application;
+﻿using axpo.reports.application;
 using Axpo;
-using CSharpFunctionalExtensions;
+using axpo.console;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Polly;
 using Serilog;
-using static System.Threading.Tasks.Task;
-using ILogger = Serilog.ILogger;
 
 static IServiceProvider ConfigServices()
 {
@@ -32,60 +28,3 @@ static IServiceProvider ConfigServices()
 var app = ConfigServices().GetRequiredService<App>();
 await app.RunAsync(new CancellationTokenSource().Token);
 
-public class App(ReportGenerator reportGenerator, ILogger logger, IConfiguration configuration)
-{
-    public async ValueTask RunAsync(CancellationToken cancellationToken)
-    {
-        var intervalInMinutes = configuration.GetValue<string>("intervalInMinutes");
-        logger.Information(
-            "Initiating application with a time interval of {Minutes} minutes",
-            intervalInMinutes
-        );
-
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            try
-            {
-                var reportData = await Policy<Result<IList<ReportData>>>
-                    .HandleResult(result => result.IsFailure)
-                    .WaitAndRetryAsync(12, sleepDurationProvider: _ => TimeSpan.FromSeconds(5), onRetry: (c, t) =>
-                    {
-                        var msg = c.Result.IsFailure ? c.Result.Error : c.Exception.Message;
-                        logger.Warning(new ApplicationException(msg), $"Failed to generate report. Retrying in {t:g}.");
-                    })
-                    .ExecuteAsync(reportGenerator.Generate);
-
-                if (reportData.IsFailure)
-                    throw new ApplicationException(reportData.Error);
-
-                CreateReportFile(reportData.Value);
-            }
-            catch (Exception e)
-            {
-                logger.Error(e, "Couldn't generate report data." );
-            }
-
-            await Delay(TimeSpan.FromMinutes(int.Parse(intervalInMinutes!)), cancellationToken);
-        }
-
-        logger.Information("Application was cancelled.");
-    }
-
-    private void CreateReportFile(IList<ReportData> data)
-    {
-        var date = TimeProvider.System.GetLocalNow();
-        var filePath = configuration.GetValue<string>("filePath");
-        Directory.CreateDirectory(filePath!);
-        var fileName = $"{filePath}PowerPosition_{date.AddDays(1):yyyyMMdd}_{date.DateTime:yyyyMMddHHmm}.csv";
-        var content = new StringBuilder();
-        content.AppendLine("Datetime;Volume");
-
-        foreach (var reportData in data)
-        {
-            content.AppendLine($"{reportData.Datetime:O};{reportData.Volume:F}");
-        }
-
-        File.WriteAllText(fileName, content.ToString());
-        logger.Information("Report {@fileName} created.", fileName);
-    }
-}
